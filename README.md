c5fbf0c51508d95e90d45105740b6b8bd4f99720
Marcin Chojnacki <Marcin_Chojnacki@epam.com>
2019-06-17 17:12:17 +0200

    add cookies

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
    а. If there is no data in cache: load from API (show loading indicator),put data into cache and show on UI
    b. if there is data in cache: Show data on UI, refresh cache with new data from API, highlight update button and show message  “new data is available click update to refresh” if data is new
 4. Extra:
-  a. show Last Updated Date (date when data cached)
+   a. show Last Updated Date (date when data cached)
    b. ignore cache if last update date is older that one hour
    
 API Example: https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=7a036c609ffa4e1485bc2cde47622085
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,9 @@
 const URL = 'https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=7a036c609ffa4e1485bc2cde47622085';
+const COOKIETIME = 1;
+
 
 function fetchData(url) {
 
-  console.log('testtt')
   displayContent();
 
   fetch(url)
@@ -13,22 +14,41 @@ function fetchData(url) {
         return myJson.message;
       }
 
-      setTimeout(() => {
-        displayContent(myJson.articles[0].author, myJson.articles[0].description);
-      }, 1000);
+      // Cookie
+      let strCookie = `author:${myJson.articles[0].author}|content:${myJson.articles[0].description}`;
+
+      if (strCookie !== getCookie('data')) {
+        setTimeout(() => {
+          displayContent(myJson.articles[0].author, myJson.articles[0].description);
+          setCookie('data', strCookie, COOKIETIME);
+          if(!!getCookie('data')) setCookieTime();
+          displayLastUpdate();
+        }, 1000);
+      } else {
+        checkDataInCookies();
+      }
+      // Our JSON has a more than 4kb
+      let data = JSON.stringify(myJson);
+
+      
+
+      // Other
+      localStorage.setItem('data', data);
+      sessionStorage.setItem('data', data);
+
+      addDataToIndexDb(myJson.articles);
+      // addNote();
 
     })
     .catch(error => console.log(error));
 
 }
 
-fetchData(URL);
+// fetchData(URL);
 // how we can do, to return json/obj. Normal is promise bcs is asyn and we no have it yet. (async/await)
 
 let button = document.querySelector('.updateBtn');
-button.addEventListener('click', function() {
-  fetchData(URL);
-}, false);
+button.addEventListener('click', () => fetchData(URL), false);
 
 
 function displayContent(author, content) {
@@ -45,6 +65,130 @@ function displayContent(author, content) {
     contentText.classList.remove('loader');
     authorText.textContent = author;
     contentText.textContent = content; 
+  }
+}
+
+//=======================
+// COOKIE
+// We can only send (in chrome)
+// 4096 bytes per cookie // 4.096 KB
+// 180 cookies per domain
+// 81920 bytes per domain* // 180*4096 = 737280 bytes. 
+//=======================
+function setCookie(name, value, minutes) {
+  let expires = '';
+  if(minutes) {
+    let date = new Date();
+    date.setTime(date.getTime() + (minutes*60*1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') {
+        c = c.substring(1,c.length);
+      }
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+  // if(document.cookie.includes(name)) {
+  //   console.log('testtt')
+  // }
+}
+
+function checkDataInCookies() {
+
+  if(!!getCookie('data')) {
+
+    let cookie = getCookie('data').split('|');
+    let arrCookie = cookie.map(item => {
+      return item.split(':');
+    });
+
+    let obj =  Object.fromEntries(arrCookie);
+
+    displayContent(obj.author, obj.content);
+
+  } else {
+    fetchData(URL);
+  }
+
+};
+
+checkDataInCookies();
+
+function setCookieTime() {
+
+  let date = new Date();
+  setCookie('dateTime', date.toLocaleString(), COOKIETIME);
+
+}
+
+function displayLastUpdate() {
+
+  let time = document.querySelector('.updateDate');
+
+  if(!!getCookie('dateTime')) {
+    time.textContent = getCookie('dateTime');
+  }
+
+};
+
+displayLastUpdate();
+
+//=======================
+// IndexDB
+//=======================
+let db = null;
+(function createDb() {
+
+  let request = indexedDB.open('data', 2);
+
+  request.onerror = function(event) {
+    console.log("Database error: " + event.target.errorCode);
+  }
+
+  request.onsuccess = function(event) {
+    db = event.target.result;
+  }
+
+  request.onupgradeneeded = function(event) {
+    db = event.target.result;
+
+    let objectStore = db.createObjectStore('articles', { autoIncrement : true });
 
   }
+
+})();
+
+function addDataToIndexDb(data) {
+
+  var customerObjectStore = db.transaction("articles", "readwrite").objectStore("articles");
+    data.forEach(function(item) {
+      if(item.author !== null) {
+        customerObjectStore.add(item);
+      } else {
+        customerObjectStore.add('No Name');
+      }
+    });
+
+}
+
+// Other example
+function addNote() {
+  const note = {
+      title: "note" + Math.random(),
+      text: "This is my note"
+  }
+  const tx = db.transaction("articles", "readwrite")
+  tx.onerror = e => alert( ` Error! ${e.target.error}  `)
+  const pNotes = tx.objectStore("articles")
+  pNotes.add(note)
 }
